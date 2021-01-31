f1e3e5192d608ea535b6565c45ade11017464357
Zohnannor <herovblin@gmail.com>
2021-01-31 22:19:50 +0300

    enter submit

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function getById(className) {
   return document.getElementById(className);
 }
 
-getById('ow1').onclick = () => {
+const calculate = () => {
   let owx1 = parseInt(getById('ow_x1').value);
   let owz1 = parseInt(getById('ow_z1').value);
   let owx2 = parseInt(getById('ow_x2').value);
@@ -18,11 +18,8 @@ getById('ow1').onclick = () => {
   }
 
   const nx1 = Math.floor((owx1 + (owx1 >= owx2 ? -128 : 128)) / 8);
-
   const nz1 = Math.floor((owz1 + (owz1 >= owz2 ? -128 : 128)) / 8);
-
   const nx2 = Math.floor((owx2 + (owx1 >= owx2 ? -128 : 128)) / 8);
-
   const nz2 = Math.floor((owz2 + (owz1 >= owz2 ? -128 : 128)) / 8);
 
   getById('n_x1').value = nx1;
@@ -30,3 +27,11 @@ getById('ow1').onclick = () => {
   getById('n_x2').value = nx2;
   getById('n_z2').value = nz2;
 };
+
+getById('ow1').onclick = calculate;
+
+document.addEventListener('keyup', e => {
+  if (e.key === 'Enter') {
+    calculate();
+  }
+});
